146049a8ee0dae26e39afd25adb292266c4ee6a9
paul-voelkle <paul.voelkle@web.de>
2024-05-02 15:48:48 +0200

    added ticklabel mapping

--- plot_utils.py
+++ plot_utils.py
@@ -52,6 +52,11 @@ class PlotSettings(DataIO):
         plt.rcParams["font.size"] = self.font_size_tick*self.scale_factor
         plt.rcParams["mathtext.default"] = "rm"
         plt.rcParams['text.usetex'] = True
+
+        try:
+            self.load('.')
+        except IOError:
+            self.load_factory()
         
     def set_scale(self):
         # print(f"Setting scale to {self.scale_factor}")
@@ -66,9 +71,26 @@ class PlotSettings(DataIO):
 
 plot_settings = PlotSettings()
 
+
 def listify(x)->list:
     if not isinstance(x, list):
         return [x]  
+
+def map_ticks(ticklabels, ticks_old):
+    # print("Mapping ticks:")
+    # print("Old tickmarks")
+    # print(ticks_old)
+
+    num_ticks = len(ticklabels)-1
+    x_start = ticks_old[0] 
+    x_stop = ticks_old[-1]
+    x_step = abs(x_start-x_stop)/num_ticks
+
+    ticks_new = np.arange(x_start, x_stop+x_step, x_step)
+ 
+    return ticks_new
+
+
 
 def create_plot(
             axs,
@@ -94,9 +116,14 @@ def create_plot(
     axs.set_yscale( Y_scale )
 
     if len(xticks) != 0:
+        xticks_old = [item for item in axs.get_xticks()]
+        axs.set_xticks(map_ticks(xticks, xticks_old))
         axs.set_xticklabels(xticks)
+
     if len(yticks) != 0:
-        axs.set_yticklabels(yticks)        
+        yticks_old = [item for item in axs.get_yticks()]
+        axs.set_yticks(map_ticks(yticks, yticks_old))
+        axs.set_yticklabels(yticks)
         
     #set title
     axs.set_title(title, fontproperties=plot_settings.titlefont )
@@ -159,7 +186,10 @@ def plot_2d(
         else:
             axs.plot(x[i], label=labels[i]) 
     
+
     create_plot(axs, fig, X_label, Y_label, X_scale, Y_scale, xticks, yticks, path, fname, title, grid)
+
+    return fig
 
 def hist(
             x:list[np.ndarray], 
@@ -208,6 +238,8 @@ def hist(
         axs.hist(x[i], bins=bins, alpha=alpha, histtype=histtype[i], density=density, label=labels[i])   
         
     create_plot(axs, fig, X_label, Y_label, X_scale, Y_scale, xticks, yticks, path, fname, title, grid)
+    
+    return fig
     
 
 def scatter(
@@ -262,6 +294,8 @@ def scatter(
     
     create_plot(axs, fig, X_label, Y_label, X_scale, Y_scale, xticks, yticks, path, fname, title, grid)
 
+    return fig
+
 def heatmap(
             x:list[np.ndarray], 
             labels:list[str]="", 
@@ -301,5 +335,71 @@ def heatmap(
     for i in range(len(x)): 
         axs.imshow(x[i], label=labels[i], cmap=cmap[i]) 
     
+    axs.set_ylim(ymin=40, ymax=0)
+    axs.set_xlim(xmin=0, xmax=40)
+
     create_plot(axs, fig, X_label, Y_label, X_scale, Y_scale, xticks, yticks, path, fname, title, grid)
 
+    return fig
+
+def multiplot(
+            cols:int,
+            rows:int, 
+            plots:list,
+            X_label:str="", 
+            Y_label:str="", 
+            X_scale="linear", 
+            Y_scale="linear",
+            xticks=[],
+            yticks=[],         
+            path='',
+            fname='',
+            title:str="",
+            grid:bool=True):
+    
+    if cols*rows != len(plots):
+        print("Error, columns*rows != len(plots)")
+        return 
+    
+    if not isinstance(plots, list):
+        plots = [plots]
+
+    fig, axs = plt.subplots(rows, cols, figsize=(plot_settings.width*cols*plot_settings.scale_factor,plot_settings.height*rows*plot_settings.scale_factor))
+
+    k = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if rows != 1 and cols != 1: 
+                axs[i,j] = plots[k]
+            elif rows == 1:
+                axs[j] = plots[k]
+            elif cols == 1:
+                axs[i] = plots[k]
+            else:
+                axs = plots[k]
+            k += 1
+
+    for ax in axs:
+        ax.set_xlabel( X_label, fontproperties=plot_settings.axislabelfont )
+        ax.set_ylabel( Y_label, fontproperties=plot_settings.axislabelfont )
+
+        ax.ticklabel_format(axis="both", style="sci", scilimits=(0,0))
+        
+        ax.set_xscale( X_scale )
+        ax.set_yscale( Y_scale )
+
+        if len(xticks) != 0:
+            ax.set_xticklabels(xticks)
+        if len(yticks) != 0:        
+            ax.set_yticklabels(yticks)
+
+        ax.legend(loc='best', prop=plot_settings.tickfont, framealpha=0.0 )
+
+    if title != "" :
+        fig.suptitle(title)
+
+    if fname != "":
+        print(f"Saving plot to {path}/{fname}")
+        os.makedirs(path, exist_ok=True)
+        plt.savefig(fname=f"{path}/{fname}")
